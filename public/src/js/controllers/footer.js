278c92d11106c5230e3115fd4aa33a60c34d1cb4
Ksusha <ks.romanchik@gmail.com>
2017-05-19 11:48:32 +0300

    delete comments

--- public/src/js/controllers/footer.js
+++ public/src/js/controllers/footer.js
@@ -3,8 +3,6 @@
 angular.module('insight.system').controller('FooterController',
   function($scope, $route, $templateCache, gettextCatalog, amMoment,  Version) {
 
-    $scope.defaultLanguage = defaultLanguage;
-
     var _getVersion = function() {
       Version.get({},
         function(res) {
@@ -13,28 +11,5 @@ angular.module('insight.system').controller('FooterController',
     };
 
     $scope.version = _getVersion();
-
-    $scope.availableLanguages = [{
-      name: 'Deutsch',
-      isoCode: 'de_DE',
-    }, {
-      name: 'English',
-      isoCode: 'en',
-    }, {
-      name: 'Spanish',
-      isoCode: 'es',
-    }, {
-      name: 'Japanese',
-      isoCode: 'ja',
-    }];
-
-    $scope.setLanguage = function(isoCode) {
-      gettextCatalog.currentLanguage = $scope.defaultLanguage = defaultLanguage = isoCode;
-      amMoment.changeLocale(isoCode);
-      localStorage.setItem('insight-language', isoCode);
-      var currentPageTemplate = $route.current.templateUrl;
-      $templateCache.remove(currentPageTemplate);
-      $route.reload();
-    };
 
   });
