f12aa90925238449526b507f3f0aa1a1087632b7
Viktor <pulyakviktor@gmail.com>
2017-10-16 19:43:30 +0300

    Fix

--- bitcore-node/index.js
+++ bitcore-node/index.js
@@ -52,6 +52,8 @@ InsightUI.prototype.setupRoutes = function(app, express) {
     if (req.headers.accept && req.headers.accept.indexOf('text/html') !== -1 &&
       req.headers["X-Requested-With"] !== 'XMLHttpRequest'
     ) {
+
+      res.setHeader('Content-Type', 'text/html');
       res.send(self.indexFile);
     }
   });
