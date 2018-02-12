dcb86faf4e427c554c0ec6bbef47c9c47865bb95
Viktor <pulyakviktor@gmail.com>
2018-02-12 10:57:18 +0300

    Bitcoin changed on QTUM in transaction statistics

--- public/src/js/controllers/statistics.js
+++ public/src/js/controllers/statistics.js
@@ -29,7 +29,7 @@ function($scope, $routeParams, Statistics, StatisticsByDaysTransactions, Statist
 
 		self.chartText = {
 			fees: gettextCatalog.getString('The total value of all transaction fees paid to miners (not including the coinbase value of block rewards).'),
-			transactions: gettextCatalog.getString('The number of daily confirmed Bitcoin transactions.'),
+			transactions: gettextCatalog.getString('The number of daily confirmed QTUM transactions.'),
 			outputs: gettextCatalog.getString('The total value of all transaction outputs per day (includes coins returned to the sender as change).'),
 			difficulty: gettextCatalog.getString('A relative measure of how difficult it is to find a new block. The difficulty is adjusted periodically as a function of how much hashing power has been deployed by the network of miners.'),
 			stakes: gettextCatalog.getString('')
